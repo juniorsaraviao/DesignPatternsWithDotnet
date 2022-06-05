ef69f5b6671e49bdd9952cad136d21b8acdccdf3
juniorsaraviao <junior.saravia.o@uni.pe>
2022-06-04 23:31:16 -0500

    Use UnitOfWork into two tables

--- DesignPatterAsp/Controllers/BeerController.cs
+++ DesignPatterAsp/Controllers/BeerController.cs
@@ -1,6 +1,9 @@
 ﻿using DesignPatternAsp.Models.ViewModels;
+using DesignPatterns.Models.Data;
 using DesignPatterns.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +23,45 @@ namespace DesignPatternAsp.Controllers
       {
          IEnumerable<BeerViewModel> beers = _unitOfWork.Beers.Get().Select(x => new BeerViewModel { Id = x.BeerId, Name = x.Name, Style = x.Style });
          return View("Index", beers);
+      }
+
+      [HttpGet]
+      public IActionResult Add()
+      {
+         var brands = _unitOfWork.Brand.Get();
+         ViewBag.Brands = new SelectList(brands, "BrandId", "Name");
+         return View();
+      }
+
+      [HttpPost]
+      public IActionResult Add(FormBeerViewModel beerViewModel)
+      {
+         if (!ModelState.IsValid)
+         {
+            var brands = _unitOfWork.Brand.Get();
+            ViewBag.Brands = new SelectList(brands, "BrandId", "Name");
+            return View("Add", beerViewModel);
+         }
+
+         var beer = new Beer { Name = beerViewModel.Name, Style = beerViewModel.Style };
+
+         if (beerViewModel.BrandId == null)
+         {
+            var brand = new Brand();
+            brand.Name = beerViewModel.OtherBrand;
+            brand.BrandId = Guid.NewGuid();
+            beer.BrandId = brand.BrandId;
+            _unitOfWork.Brand.Add(brand);
+         }
+         else
+         {
+            beer.BrandId = (Guid)beerViewModel.BrandId;
+         }
+
+         _unitOfWork.Beers.Add(beer);
+         _unitOfWork.Save();
+
+         return RedirectToAction("Index");
       }
    }
 }
