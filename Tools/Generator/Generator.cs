131beb90971fdc7093cc590eec2286667f4c524f
juniorsaraviao <junior.saravia.o@uni.pe>
2022-06-06 23:22:42 -0500

    Create builder structure

---
+++ Tools/Generator/Generator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Tools.Generator
+{
+   public class Generator
+   {
+      public List<string> Content { get; set; }
+      public string Path { get; set; }
+      public TypeFormat Format { get; set; }
+      public TypeCharacter Character { get; set; }
+
+      public void Save()
+      {
+         string result = "";
+         result = Format == TypeFormat.Json ? GetJson() : GetPipe();
+         switch(Character) {
+            case TypeCharacter.Uppercase:
+               result.ToUpper();
+               break;
+            case TypeCharacter.Lowercase:
+               result.ToLower();
+               break;
+         }
+
+         File.WriteAllText(Path, result);
+      }
+
+      string GetJson()
+      {
+         return JsonSerializer.Serialize(Content);
+      }
+
+      string GetPipe()
+      {
+         return Content.Aggregate((accum, current) => $"{accum}|{current}");
+      }
+   }
+}
---
+++ Tools/Generator/GeneratorDirector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Generator
+{
+   public class GeneratorDirector
+   {
+      private IBuilderGenerator _generator;
+
+      public GeneratorDirector(IBuilderGenerator generator)
+      {
+         SetBuilder(generator);
+      }
+
+      public void SetBuilder(IBuilderGenerator generator)
+      {
+         _generator = generator;
+      }
+
+      public void CreateSimpleJsonGenerator(List<string> content, string path)
+      {
+         _generator.Reset();
+         _generator.SetContent(content);
+         _generator.SetPath(path);
+         _generator.SetFormat(TypeFormat.Json);
+      }
+
+      public void CreateSimplePipeGenerator(List<string> content, string path)
+      {
+         _generator.Reset();
+         _generator.SetContent(content);
+         _generator.SetPath(path);
+         _generator.SetFormat(TypeFormat.Pipes);
+         _generator.SetCharacter(TypeCharacter.Uppercase);
+      }
+   }
+}
